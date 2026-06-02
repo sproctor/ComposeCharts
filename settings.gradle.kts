af2ffcaab047b530f7d6c1fa60955448a7eeb5af
Sean Proctor <sproctor@gmail.com>
2026-06-02 04:55:48 -0400

    add refreshVersions plugin and configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,17 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+plugins {
+    // See https://splitties.github.io/refreshVersions/
+    id("de.fayard.refreshVersions") version "0.60.6"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+refreshVersions {
+    rejectVersionIf {
+        candidate.stabilityLevel.isLessStableThan(current.stabilityLevel)
+    }
+}
 
 rootProject.name = "ComposeCharts"
 include(":app")
